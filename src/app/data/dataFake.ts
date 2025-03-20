db865742e26c7290e67fd30c97a98e1989fd6284
Danilo Acioly <danilofcode01@gmail.com>
2025-03-20 19:21:25 -0300

    Feat: incluindo alguns styles

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Novo jogo: POKÉMON LEGENDS Z/A ANUNCIADO!",
+    "description": "Descubram uma nova aventura na Lumiose City, onde um plano de desenvolvimento urbano transformará esta cidade num local onde as pessoas e os Pokémon coexistem em harmonia.",
+    "photoCover":"https://legends.pokemon.com/_images/feb_27_2025/trailer_thumbnail_en.png"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Tudo Sobre POKÉMON LEGENDS Z/A: Análise Completa do Trailer",
+    "description": "Novo sistema de Mega Evolução? AZ retornando ? Confira, na íntegra, os detalhes do trailer que você não percebeu!",
+    "photoCover":"https://i.ytimg.com/vi/7RLJZn6VSz8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCH2qchFD6-E4FeCtNxYzI8m7WYOA"
+  },
+  {
+    "id":"3",
+    "title": "Novidades sobre o Pokémon World Championship 2025",
+    "description": "O evento reunirá competidores das modalidades TCG (Trading Card Game), VGC (Video Game Championship), Pokémon UNITE e Pokémon GO",
+    "photoCover":"https://static1.srcdn.com/wordpress/wp-content/uploads/2025/02/pokemon-eiuc-hed.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Nintendo Switch 2: Veja as novas mudanças do console!",
+    "description": "Novos detalhes do console deverão ser apresentados durante o Nintendo Direct que acontecerá no dia 2 de abril",
+    "photoCover":"https://abcagora.com.br/wp-content/uploads/2025/01/Nintendo-anuncia-Switch-2-com-compatibilidade-com-jogos-do-modelo-original-e-novo-Mario-Kart.webp"
   }
 ]
